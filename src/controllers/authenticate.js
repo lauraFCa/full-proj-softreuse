2f44ab4e4770e298cc10c2ae03f1303936406127
Laura F Ca <laurafariacastro@gmail.com>
2024-04-24 01:41:52 -0300

    Fix code for lint

--- src/controllers/authenticate.js
+++ src/controllers/authenticate.js
@@ -1,6 +1,6 @@
-import jwt from 'jsonwebtoken';
+import jwt from 'jsonwebtoken'
 
-const secretKey = 'A1B2C3D4E5F6G7H8';
+const secretKey = 'A1B2C3D4E5F6G7H8'
 
 /**
  * Generates a JWT token for the given user ID.
@@ -8,9 +8,9 @@ const secretKey = 'A1B2C3D4E5F6G7H8';
  * @returns {string} The generated JWT token.
  */
 export const generateToken = (userId) => {
-    console.log({ userId });
-    return jwt.sign({ userId }, secretKey, { expiresIn: '10h' });
-};
+  console.log({ userId })
+  return jwt.sign({ userId }, secretKey, { expiresIn: '10h' })
+}
 
 /**
  * Middleware function to authenticate a JWT token.
@@ -19,19 +19,18 @@ export const generateToken = (userId) => {
  * @param {Function} next - The next middleware function.
  */
 export const authenticateToken = (req, res, next) => {
-    const token = req.headers['authorization'];
-    if (!token) {
-        return res.status(401).json({ message: 'Token de autenticacao nao fornecido' });
+  const token = req.headers.authorization
+  if (!token) {
+    return res.status(401).json({ message: 'Token de autenticacao nao fornecido' })
+  }
+
+  jwt.verify(token, secretKey, (err, decoded) => {
+    if (err) {
+      console.log(err)
+      return res.status(403).json({ message: 'Token de autenticacao invalido' })
     }
-
-    jwt.verify(token, secretKey, (err, decoded) => {
-        if (err) {
-            console.log(err);
-            return res.status(403).json({ message: 'Token de autenticacao invalido' });
-        }
-        req.userId = decoded.userId;
-        console.log(req.userId);
-        next();
-    });
-};
-
+    req.userId = decoded.userId
+    console.log(req.userId)
+    next()
+  })
+}
